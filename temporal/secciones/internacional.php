384886999fe555196c5abc0e16f0edc6377127ab
Fabrizio <68043260+F4brizio@users.noreply.github.com>
2024-01-09 22:18:02 -0500

    Update internacional.php

--- temporal/secciones/internacional.php
+++ temporal/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Protestas masivas en Europa por políticas climáticas",
+    "autor" => "Juan Pérez",
+    "resumen" => "Miles de personas se han congregado en ciudades europeas principales para exigir medidas urgentes en la lucha contra el cambio climático. Las protestas pacíficas buscan presionar a los líderes mundiales para tomar acciones concretas y significativas.",
 ];
 ?>
